499ce1a1d73b4853a7e36d4bf867a32d14a674c7
shupopo <saihatenoasa@gmail.com>
2019-01-07 22:16:56 +0900

    first commit

---
+++ resources/main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        product: "socks",
+        expression: "wwww",
+        image:"./socks.jpeg",
+        link:"http://www.google.com"
+    }
+});
+
