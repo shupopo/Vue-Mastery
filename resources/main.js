f3d2ca6c0da4defc2e01e73ec09e03dce159f32c
shupopo <saihatenoasa@gmail.com>
2019-01-19 15:39:12 +0900

    finish lesson 6

--- resources/main.js
+++ resources/main.js
@@ -1,33 +1,40 @@
 var app = new Vue({
     el: "#app",
     data: {
+        brand: "Vue mastery",
         product: "socks",
+        selectedVariant: 0,
         expression: "wwww",
-        image:"./socks.jpeg",
-        link:"http://www.google.com",
-        inStock :false,
-        inventory : 9,
-        onSale:true,
-        details:["www","big","cool"],
-        cart:0,
-        variants:[
+        image: "./socks.jpeg",
+        link: "http://www.google.com",
+        inStock: false,
+        inventory: 9,
+        onSale: true,
+        details: ["www", "big", "cool"],
+        cart: 0,
+        variants: [
             {
-                variantId:2234,
-                variantColor:"green",
-                variantImage:"./socks.jpeg"
-            },{
-                variantId:2235,
-                variantColor:"blue",
-                variantImage:"./bluesocks.jpeg"
+                variantId: 2234,
+                variantColor: "green",
+                variantImage: "./socks.jpeg"
+            }, {
+                variantId: 2235,
+                variantColor: "blue",
+                variantImage: "./bluesocks.jpeg"
             }
         ]
     },
-    methods:{
-        addToCart:function(){
+    methods: {
+        addToCart: function () {
             this.cart += 1
         },
-        updateProduct:function(variantImage){
-            this.image=variantImage
+        updateProduct: function (variantImage) {
+            this.image = variantImage
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
         }
     }
 
